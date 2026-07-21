75f74b892e6287c0521ec23f854428c786ecfe6a
gHenshall <gareth.henshall@gmail.com>
2026-07-21 16:36:03 +0100

    Far simpler concept. added content from old site

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,15 +1,13 @@
 import { defineCollection, z } from 'astro:content';
 
-const newsCollection = defineCollection({
+const articlesCollection = defineCollection({
   schema: z.object({
     title: z.string(),
     date: z.date(),
-    featured: z.boolean().default(false),
-    emoji: z.string(),
     excerpt: z.string(),
   }),
 });
 
 export const collections = {
-  news: newsCollection,
+  articles: articlesCollection,
 };
