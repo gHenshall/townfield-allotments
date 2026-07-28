d2590181f2c631d626eb0071da097a278c3af2f9
gHenshall <gareth.henshall@gmail.com>
2026-07-28 14:11:17 +0100

    updating gallery setup

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,13 +1,24 @@
 import { defineCollection, z } from 'astro:content';
 
-const articlesCollection = defineCollection({
+const news = defineCollection({
   schema: z.object({
     title: z.string(),
-    date: z.date(),
+    date: z.coerce.date(),
     excerpt: z.string(),
+    image: z.string(),
+    featured: z.boolean().optional().default(false),
   }),
 });
 
-export const collections = {
-  articles: articlesCollection,
-};
+const galleries = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    date: z.coerce.date(),
+    excerpt: z.string(),
+    coverImage: z.string(),
+    images: z.array(z.string()),
+    featured: z.boolean().optional().default(false),
+  }),
+});
+
+export const collections = { news, galleries };
